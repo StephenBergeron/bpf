8331eaf339e344feb8efcfc95aa8871a645ff460
Bergeron, Stephen <Stephen.Bergeron@Kronos.com>
2018-05-25 10:20:58 -0400

    Read data from the tsv file. I use a sample file for now. 	modified:   src/bpf/core.clj 	modified:   test/bpf/core_test.clj

--- src/bpf/core.clj
+++ src/bpf/core.clj
@@ -1,5 +1,20 @@
 (ns bpf.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure-csv.core :as clojure-csv.core]))
+
+(defn nth-colum
+  [tabl column]
+  (remove nil? (map (fn [x] (nth x column nil)) tabl)))
+
+(def bj-file-name
+  (str "/home/stn/cache/wfm87/" "53568.dat"))
+
+(def bj
+  (clojure-csv.core/parse-csv (slurp bj-file-name) :delimiter \tab))
+
+(def startdtm (nth-colum bj 17))
+(def enddtm (nth-colum bj 15))
+(def launchdtm (nth-colum bj 3))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/bpf/core_test.clj
+++ test/bpf/core_test.clj
@@ -2,6 +2,25 @@
   (:require [clojure.test :refer :all]
             [bpf.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def ^{:private true} bj-file
+  (clojure.java.io/file bj-file-name))
+
+(deftest bj-file-exist
+  (testing (is (.exists bj-file))))
+
+(deftest bj-file-length
+  (testing (is (< 5 (.length bj-file)))))
+
+(deftest bj-count
+  (testing (is (< 0 (count bj)))))
+
+(deftest startdtm-count
+  (testing (is (< 0 (count startdtm)))))
+
+;; (deftest content-valid
+;;   (testing (is (= "hello" enddtm ))))
+
+(deftest generic-can-read-tab-delimiter
+  (testing
+    (is (= [["First", "Second"]]
+           (clojure-csv.core/parse-csv "First\tSecond" :delimiter \tab)))))
