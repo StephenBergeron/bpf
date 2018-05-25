149b1870336201fcea9a7ecaec555ac1b7f3fb73
Bergeron, Stephen <Stephen.Bergeron@Kronos.com>
2018-05-25 08:43:32 -0400

    First thing: add lein-ancient and lein-auto plugins. 	modified:   project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject bpf "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Simple utility tool for processing bpf data in TSV format"
+  :url "http://too.poor.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [clojure-csv/clojure-csv "2.0.2"]]
+  :plugins [[lein-auto "0.1.3"] [lein-ancient "0.6.15"]]
   :main ^:skip-aot bpf.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
